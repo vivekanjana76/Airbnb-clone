01b35b4589ad6bd95f724117e4503ba2bec5e104
vivek anjana <vivekanjana76@gmail.com>
2023-11-02 08:17:42 +0530

    Full Airbnb-clone Completed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,43 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: 'res.cloudinary.com' },
+      { hostname: 'avatars.githubusercontent.com' },
+      { hostname: 'lh3.googleusercontent.com' },
+      // Add any other remote patterns you need
+    ],
+  },
+};
+
+module.exports = nextConfig;
+
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     // experimental: {
+//     //   appDir: true,
+//     // },
+//     images: {
+//       domains: [
+//         'res.cloudinary.com', 
+//         'avatars.githubusercontent.com',
+//         'lh3.googleusercontent.com'
+//       ]
+//     }
+//   }
+  
+//   module.exports = nextConfig
+
+
+  // images: {
+  //   domains: [
+  //     'res.cloudinary.com', 
+  //     'avatars.githubusercontent.com',
+  //     'lh3.googleusercontent.com'
+  //   ]
+  // }
