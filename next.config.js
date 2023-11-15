bff215293059bd2b0ef6c1a4f1a3370d28378da2
vivek anjana <vivekanjana76@gmail.com>
2023-11-15 11:52:43 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,43 +1,33 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      { hostname: 'res.cloudinary.com' },
-      { hostname: 'avatars.githubusercontent.com' },
-      { hostname: 'lh3.googleusercontent.com' },
-      // Add any other remote patterns you need
-    ],
-  },
-};
-
-module.exports = nextConfig;
-
-
-
-
+// // next.config.js
 // /** @type {import('next').NextConfig} */
+
 // const nextConfig = {
-//     // experimental: {
-//     //   appDir: true,
-//     // },
-//     images: {
-//       domains: [
-//         'res.cloudinary.com', 
-//         'avatars.githubusercontent.com',
-//         'lh3.googleusercontent.com'
-//       ]
-//     }
-//   }
-  
-//   module.exports = nextConfig
+//   images: {
+//     remotePatterns: [
+//       { hostname: 'res.cloudinary.com' },
+//       { hostname: 'avatars.githubusercontent.com' },
+//       { hostname: 'lh3.googleusercontent.com' },
+//       // Add any other remote patterns you need
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
 
 
-  // images: {
-  //   domains: [
-  //     'res.cloudinary.com', 
-  //     'avatars.githubusercontent.com',
-  //     'lh3.googleusercontent.com'
-  //   ]
-  // }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: [
+      'res.cloudinary.com', 
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com'
+    ]
+  }
+}
+
+module.exports = nextConfig
+
